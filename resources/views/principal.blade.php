26690616451c48dafc577a5ab3f1db2382f26f7e
cauaandradevieira <cauaandradevieiraa@gmail.com>
2026-02-06 20:42:07 -0300

    .

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto de Teste</title>
 
-   <!-- Vite cuida do CSS/JS -->
-    @vite('resources/css/app.css')
-
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
+
 <body class="bg-gray-100">
 
     <header>
@@ -22,4 +21,7 @@
     </main>
 
 </body>
+<!-- Tailwind via CDN -->
+<script src="https://cdn.tailwindcss.com"></script>
+
 </html>
