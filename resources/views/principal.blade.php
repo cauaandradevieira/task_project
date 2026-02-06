d48b33757648b69d3c03646ff700f2679aadbb15
cauaandradevieira <cauaandradevieiraa@gmail.com>
2026-02-06 19:17:17 -0300

    resolvendo erro css

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto de Testea</title>
-    @vite(['resources/css/app.css', 'resources/css/style.css'])
+
+    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
